89cdd84babfc86d7856870b889eda17a00648f05
Kng-T <traipop.py@gmail.com>
2026-04-03 09:58:54 +0700

    update

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -49,7 +49,8 @@ export default function HeroSection({ onStart }: HeroSectionProps) {
 
         <h1 className="text-xl font-bold leading-tight tracking-tight sm:text-2xl md:text-3xl">
           ประชาพิจารณ์ <br />
-          <span className="text-fuchsia-500"> ร่างประกาศองค์การบริหารส่วนจังหวัดเชียงราย เรื่อง การงดจัดเก็บภาษีน้ำมัน</span>
+          <span className="text-shadow-fuchsia-400"> ร่างประกาศองค์การบริหารส่วนจังหวัดเชียงราย <br /> 
+          เรื่อง การงดจัดเก็บภาษีน้ำมัน</span>
           <br />
           เพื่อชาวเชียงราย
         </h1>
